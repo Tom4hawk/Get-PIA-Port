9129f85d08b52e3f1fc89d8e260d36797437098c
Tom4hawk <tom4hawk@tom4hawk.com>
2015-10-16 02:15:19 +0200

    Now indentation look the way I like it

--- getpiaport.sh
+++ getpiaport.sh
@@ -8,48 +8,44 @@
 # Usage:
 #  ./port_forward.sh <user> <password>
 
-error( )
-{
-  echo "$@" 1>&2
-  exit 1
+error(){
+	echo "$@" 1>&2
+	exit 1
 }
 
-error_and_usage( )
-{
-  echo "$@" 1>&2
-  usage_and_exit 1
+error_and_usage(){
+	echo "$@" 1>&2
+	usage_and_exit 1
 }
 
-usage( )
-{
-  echo "Usage: `dirname $0`/$PROGRAM <user> <password>"
+usage(){
+	echo "Usage: `dirname $0`/$PROGRAM <user> <password>"
 }
 
-usage_and_exit( )
-{
-  usage
-  exit $1
+usage_and_exit(){
+	usage
+	exit $1
 }
 
-version( )
-{
-  echo "$PROGRAM version $VERSION"
+version(){
+	echo "$PROGRAM version $VERSION"
 }
 
 
-port_forward_assignment( )
-{
-  echo 'Loading port forward assignment information..'
-  if [ "$(uname)" == "Linux" ]; then
-    local_ip=`ip addr show tun0|grep -oE "inet *10\.[0-9]+\.[0-9]+\.[0-9]+"|tr -d "a-z "`
-    client_id=`head -n 100 /dev/urandom | md5sum | tr -d " -"`
-  fi
-  if [ "$(uname)" == "Darwin" ]; then
-    local_ip=`ifconfig tun0 | grep "inet " | cut -d\  -f2|tee /tmp/vpn_ip`
-    client_id=`head -n 100 /dev/urandom | md5 -r | tr -d " -"`
-  fi
-  json=`curl --silent --data "user=$USER&pass=$PASSWORD&client_id=$client_id&local_ip=$local_ip" -o - 'https://www.privateinternetaccess.com/vpninfo/port_forward_assignment' | head -1 | grep -o '[0-9]*'`
-  echo $json
+port_forward_assignment(){
+	echo 'Loading port forward assignment information..'
+	if [ "$(uname)" == "Linux" ]; then
+		local_ip=`ip addr show tun0|grep -oE "inet *10\.[0-9]+\.[0-9]+\.[0-9]+"|tr -d "a-z "`
+		client_id=`head -n 100 /dev/urandom | md5sum | tr -d " -"`
+	fi
+
+	if [ "$(uname)" == "Darwin" ]; then
+		local_ip=`ifconfig tun0 | grep "inet " | cut -d\  -f2|tee /tmp/vpn_ip`
+		client_id=`head -n 100 /dev/urandom | md5 -r | tr -d " -"`
+	fi
+	
+	json=`curl --silent --data "user=$USER&pass=$PASSWORD&client_id=$client_id&local_ip=$local_ip" -o - 'https://www.privateinternetaccess.com/vpninfo/port_forward_assignment' | head -1 | grep -o '[0-9]*'`
+	echo $json
 }
 
 EXITCODE=0
@@ -60,19 +56,19 @@ PASSWORD=$2
 
 while test $# -lt 2
 do
-  case $1 in
-  --usage | --help | -h )
-    usage_and_exit 0
-    ;;
-  --version | -v )
-    version
-    exit 0
-    ;;
-  *)
-    error_and_usage "Unrecognized option: $1"
-    ;;
-  esac
-  shift
+	case $1 in
+		--usage | --help | -h )
+			usage_and_exit 0
+		;;
+		--version | -v )
+			version
+			exit 0
+		;;
+	*)
+		error_and_usage "Unrecognized option: $1"
+		;;
+	esac
+	shift
 done
 
 port_forward_assignment
